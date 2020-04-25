f691fa0952888f08d47c1cac9488c5cf17b05b47
YAMASHITA Takao <tjy1965@gmail.com>
2020-04-25 20:33:58 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,23 +2,18 @@
 
 for f in $(cat <<EOF
 .Brewfile
-.emacs.d
-.git
 .gitconfig
 .gitignore
 .gitmodules
-.setup.sh.swp
 .vim
 .vimperator
 .vimperatorrc
 .vimperatorrc.js
 bin
-iterm2-colors
-setup.sh
 EOF
 ); do
 	test -f $f -o -d $f && (
 		echo -- $f
-		ln -fs $f ~
+		ln -fs $(pwd)/$f ~/.
 	)
 done

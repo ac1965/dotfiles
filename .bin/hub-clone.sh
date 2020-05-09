29202db44b05521ab259e6bc21399b35e72a362f
YAMASHITA Takao <tjy1965@gmail.com>
2020-05-09 17:56:25 +0900

    update

---
+++ .bin/hub-clone.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+T="${HOME}/Documents/devel/src"
+
+if [ $# -eq 1 ]; then
+    url=$1
+    git=$(echo $url|sed 's#http.*://##g')
+    host=$(echo $git| awk -F'/' '{print $1}')
+    owner=$(echo $git| awk -F'/' '{print $2}')
+    repos=$(echo $git| awk -F'/' '{print $3}')
+    #echo "url:$url, owner=$owner, repos=$repos"
+    test -d ${T}/${host} || install -d ${T}/${host}
+    test -d ${T}/${host}/${owner} || install -d ${T}/${host}/${owner}
+    (
+        cd ${T}/${host}/${owner}
+        if [ -d ${repos} ]; then
+            cd ${repos} && git pull
+        else
+            git clone --recursive ${url}
+        fi
+    )
+else
+    echo $0 url
+    exit
+fi

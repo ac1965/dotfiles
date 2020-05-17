fd4eced1072e6dd48c12699bb33bb7cee7033e3a
YAMASHITA Takao <tjy1965@gmail.com>
2020-05-17 11:36:24 +0900

    feat(emacs): Emacsビルドスクリプトの追加とREADME更新
    - `build-emacs.sh` を新規追加（リポジトリの取得・更新・ビルドを自動化）
    - `configure` オプションから `--with-ns` を削除
    - `sudo make install` を `make install` に変更
    - インストール後の手順を `open -R nextstep/Emacs.app` に変更

---
+++ build-emacs.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+# Prequisites
+# - Xcode
+# - git
+# - imagemagick (allows image viewing)
+# - gnutls (allows communication via SSL, TLS, amd DTLS)
+# - autoconf
+# - automake
+
+DEST="$HOME/Documents/devel/src/github.com/emacs-mirror/emacs"
+APPS="/Applications/Emacs.app"
+
+if [ -d $DEST ]; then
+    cd $DEST
+    git reset --hard
+    git clean -xdf
+    git pull
+else
+    ~/.bin/hub-clone.sh https://github.com/emacs-mirror/emacs.git
+fi
+
+cd $DEST
+./autogen.sh  && \
+    CFLAGS=`xml2-config --cflags` ./configure && make install && (
+        test -d $APPS && rm -fr $APPS
+        open -R nextstep/Emacs.app
+    )

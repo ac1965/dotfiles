286d1918151ddc4f94ece37360a81598bf384d27
YAMASHITA Takao <tjy1965@gmail.com>
2020-05-30 15:26:02 +0900

    update

--- .bin/hub-update.sh
+++ .bin/hub-update.sh
@@ -2,14 +2,16 @@
 
 THIS="$HOME/Documents/devel/src"
 TARGET="${THIS}/${1:-${GIT_DEFAULT_SITE}}"
+LOG="${THIS}/hub-update.log"
 
 test -d $TARGET && (
     cd $TARGET
-    pwd
+    test -f $LOG && rm -f $LOG
     for d in $(gls --color=none)
     do
         (
         cd $d
+        test -x ~/.bin/hub-repos.sh && ~/.bin/hub-repos.sh $(pwd | awk -F'/' '{print $NF}') > repos-${d}.txt
         for g in $(gls --color=none -l | grep "^d" | awk '{print $NF}')
         do
             (
@@ -19,7 +21,7 @@ test -d $TARGET && (
                 git pull
             )
         done
-        )
+        ) | tee -a $LOG
     done
 ) || echo $0 git-site
 

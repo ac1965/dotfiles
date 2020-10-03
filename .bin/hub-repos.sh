0439e116458cb3fb197304c6806f332c1ec2db70
YAMASHITA Takao <tjy1965@gmail.com>
2020-10-03 18:58:58 +0900

    update

--- .bin/hub-repos.sh
+++ .bin/hub-repos.sh
@@ -5,6 +5,6 @@ set -o noclobber
 
 GITHUB_TOKEN=${GITHUB_TOKEN:-""}
 GITHUB_APIURL="https://api.github.com"
-GITHUB_API="/users/${1:-${SNS_USERNAME}}/repos"
+GITHUB_API="/users/${1:-${SNS_USERNAME}}/repos?per_page=100"
 
 curl -k -s -u :${GITHUB_TOKEN} ${GITHUB_APIURL}${GITHUB_API} 2>/dev/null | jq -r '.[].name'

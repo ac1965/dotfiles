14af195ca14bd6eca009fd619bd3b84a0c4213c5
YAMASHITA Takao <tjy1965@gmail.com>
2024-02-10 17:22:15 +0900

    refactor(.bin): hub-clone.shの改善とhub-update.shの削除
    - hub-clone.shのデバッグ用echoコメントを解除
    - reposが空文字の場合にgit操作をスキップする条件分岐を追加
    - hub-update.shスクリプトを削除

--- .bin/hub-clone.sh
+++ .bin/hub-clone.sh
@@ -8,17 +8,19 @@ if [ $# -eq 1 ]; then
     host=$(echo $git| awk -F'/' '{print $1}')
     owner=$(echo $git| awk -F'/' '{print $2}')
     repos=$(echo $git| awk -F'/' '{print $3}')
-    #echo "url:$url, owner=$owner, repos=$repos"
+    echo "url:$url, owner=$owner, repos=$repos"
     test -d ${T}/${host}/${owner} || install -d ${T}/${host}/${owner}
     test -x ~/.bin/hub-repos.sh && ~/.bin/hub-repos.sh $owner > ${T}/${host}/${owner}/repos-${owner}.txt
-    (
-        cd ${T}/${host}/${owner}
-        if [ -d ${repos} ]; then
-            cd ${repos} && git pull
-        else
-            git clone --recursive ${url}
-        fi
-    )
+    if [ "${repos}" != "" ]; then
+        (
+            cd ${T}/${host}/${owner}
+            if [ -d ${repos} ]; then
+                cd ${repos} && git pull
+            else
+                git clone --recursive ${url}
+            fi
+        )
+    fi
 else
     echo $0 url
     exit
